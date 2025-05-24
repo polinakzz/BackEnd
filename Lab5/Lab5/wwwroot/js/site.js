a3a7298033edeef564d79e333fd9154252dfb510
polinakzz <www@example.com>
2025-05-24 13:53:45 +0300

    Добавлена лабораторная работа 5

---
+++ Lab5/Lab5/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    console.log("JavaScript подключен и работает");
+
+    const message = document.getElementById("js-message");
+    if (message) {
+        message.textContent = "Прив(site.js)";
+        message.style.color = "green";
+    }
+});
