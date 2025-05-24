9b52e2cbe427596e5802a4e366a12bbd60a7fecb
polinakzz <www@example.com>
2025-05-24 15:00:32 +0300

    Добавлена лабораторная работа 8

---
+++ Lab8/Lab8/Controllers/StateController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab8.Controllers
+{
+    public class StateController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Читаем имя из сессии и cookie
+            ViewBag.SessionName = HttpContext.Session.GetString("UserName");
+            ViewBag.CookieName = Request.Cookies["UserName"];
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save(string name)
+        {
+            // Сохраняем в сессию
+            HttpContext.Session.SetString("UserName", name);
+
+            // Сохраняем в cookie
+            Response.Cookies.Append("UserName", name, new CookieOptions
+            {
+                Expires = DateTimeOffset.UtcNow.AddMinutes(30)
+            });
+
+            return RedirectToAction("Index");
+        }
+    }
+}
